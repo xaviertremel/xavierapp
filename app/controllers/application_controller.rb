4eb2fbae583a9e1738dcb8a5bada1ffe3b7486da
Xavier <xavier.tremel@gmail.com>
2017-01-12 15:13:15 -0200

    authentication forms created

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_filter :logged_user
+
+  def logged_user
+  	if user_signed_in?
+  		@current_user = current_user.email
+  	end
+  end
+
 end
