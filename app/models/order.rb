ad08853a81aad7a99cd8853c6050a01fbaf8ddf4
Xavier <xavier.tremel@gmail.com>
2017-01-13 16:36:34 -0200

    Comment Model added

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,7 +2,3 @@ class Order < ApplicationRecord
   belongs_to :product
   belongs_to :user
 end
-
-class Product < ApplicationRecord
-  has_many :orders
-end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,13 @@
 class Product < ApplicationRecord
+	has_many :orders
+	has_many :comments
+
+	def highest_rating_comment
+  	comments.rating_desc.first
+	end
+
+	def lowest_rating_comment
+		comments.rating_desc.last
+	end
+
 end
