29f368d1e82f0c79fb94e57fdb2d4bb5f8439a87
Xavier <xavier.tremel@gmail.com>
2017-01-24 15:22:25 -0200

    elevateZoom plugin added

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -1,4 +1,6 @@
 $(document).on('turbolinks:load', function(){
+
+		//Raty
     $('.rating').raty( { path: '/assets', scoreName: 'comment[rating]' });
     $('.rated').raty({ path: '/assets',
       readOnly: true,
@@ -6,4 +8,21 @@ $(document).on('turbolinks:load', function(){
         return $(this).attr('data-score');
       }
     });
+
+    //elevateZoom
+    var width = $(window).width();
+    if (width < 700) {
+    		var position = 14
+    	}
+    	else {
+    		var position = 10
+    	};
+
+    $('#image-zoom').elevateZoom({
+    	zoomWindowPosition: position,
+  		easing: "true",
+  		borderSize: 0,
+  		zoomWindowOffetx: -10
+		});
+
 });
