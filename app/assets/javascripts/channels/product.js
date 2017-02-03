9900f696b40d9672fea73d7afa523d6cf195f170
Xavier <xavier.tremel@gmail.com>
2017-02-03 15:11:41 -0200

    test

--- app/assets/javascripts/channels/product.js
+++ app/assets/javascripts/channels/product.js
@@ -11,7 +11,7 @@ App.product = App.cable.subscriptions.create("ProductChannel", {
     // Called when there's incoming data on the websocket for this channel
     $("#cable-notice").html(data.comment).fadeIn(1000);
     $("#cable-notice").html(data.comment).fadeOut(6000);
-    $('.product-reviews').prepend(data.comment);
+    //$('.product-reviews').prepend(data.comment);
     $("#average-rating").attr('data-score', data.average_rating);
     refreshRating();
     console.log(data);
